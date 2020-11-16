04cb1dd7643d3b55f7c6c396eb9cd12b5ed4079a
Sarthak Mohapatra <Sarthak.Mohapatra@diconium.com>
2020-11-16 20:32:15 +0530

    initial commit to add the project

---
+++ learn-springcloud-config-server/src/main/java/com/learn/springcloud/config/server/learnspringcloudconfigserver/LearnSpringcloudConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.config.server.learnspringcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringcloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringcloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ learn-springcloud-config-server/src/test/java/com/learn/springcloud/config/server/learnspringcloudconfigserver/LearnSpringcloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springcloud.config.server.learnspringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringcloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
